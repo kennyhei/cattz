c5e9d48b6238040f8631d27f770fba6dbae3eaee
Kenny Heinonen <kenny_heinonen@hotmail.com>
2013-10-29 13:53:54 +0200

    Remove blocks that were hit - #5

--- src/game/GameApp.java
+++ src/game/GameApp.java
@@ -140,14 +140,9 @@ public class GameApp extends SimpleApplication {
         
         blockNode.collideWith(ray, results);
 
-        System.out.println("----- Collisions? " + results.size() + "-----");
+        // Remove blocks that were hit by the player
         for (int i = 0; i < results.size(); i++) {
-            // For each hit, we know distance, impact point, name of geometry.
-            float dist = results.getCollision(i).getDistance();
-            Vector3f pt = results.getCollision(i).getContactPoint();
-            String hit = results.getCollision(i).getGeometry().getName();
-            System.out.println("* Collision #" + i);
-            System.out.println("  You hit " + hit + " at " + pt + ", " + dist + " wu away.");
+            blockNode.detachChild(results.getCollision(i).getGeometry());
         }
     }
 
